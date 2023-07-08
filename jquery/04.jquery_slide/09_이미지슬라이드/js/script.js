54e51f1149732357181ce21e31909d8adf01eaa2
pyj <youjun0216@naver.com>
2023-07-08 16:12:12 +0900

    first commit

---
+++ jquery/04.jquery_slide/09_이미지슬라이드/js/script.js
@@ -0,0 +1,56 @@
+let current=0;
+const banner=$('#wrap .slideUl li');
+const btn=$('.buttonList li');
+let setIntervalId;
+
+slide()
+function slide(){
+    setIntervalId=setInterval(function(){
+        let prev=banner.eq(current);
+        let prevBtn=btn.eq(current)
+        move(prev, 0, '-100%')
+        prevBtn.removeClass('on')
+        current++;
+        console.log(current)
+        if(current==banner.length) current=0;
+        let next=banner.eq(current)
+        let nextBtn=btn.eq(current)
+        nextBtn.addClass('on')
+        move(next, '100%', 0)
+    },2000)
+}
+
+$('.slide').on({mouseover:function(){
+    clearInterval(setIntervalId)
+},mouseout:function(){
+    slide()
+}})
+function move(tg, start, end){
+    tg.css('left',start).stop().animate({left:end},500)
+}
+
+$('.next').on('click', function(){
+    let prev=banner.eq(current);
+    let prevBtn=btn.eq(current)
+    move(prev, 0, '-100%')
+    prevBtn.removeClass('on')
+    current++;
+    if(current==banner.length) current=0;
+    let next=banner.eq(current)
+    let nextBtn=btn.eq(current)
+    nextBtn.addClass('on')
+    move(next, '100%', 0)
+});
+$('.prev').on('click', function(){
+    let prev=banner.eq(current);
+    let prevBtn=btn.eq(current)
+    move(prev, 0, '100%')
+    prevBtn.removeClass('on')
+    current--;
+  
+    if(current==-banner.length) current=0;
+    let next=banner.eq(current)
+    let nextBtn=btn.eq(current)
+    move(next, '-100%', 0)
+    nextBtn.addClass('on')
+});
